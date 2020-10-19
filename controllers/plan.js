c79ffffae089a379b6db7101efd032787ed15d28
Gustaff <gustavohmfigueiredo@gmail.com>
2020-10-19 16:29:40 -0300

    cleaning plans file

--- controllers/plan.js
+++ controllers/plan.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Patient, Plan, PatientSurgeries, Surgerie } = require('../models');
+const { Patient, Plan } = require('../models');
 
 const plansRouter = express.Router();
 
@@ -8,19 +8,7 @@ const getPatientsById = (req, res, next) => Plan
   .then((planWithPatients) => res.status(200).json(planWithPatients))
   .catch((e) => next(e.message));
 
-const getAllWithSurgeries = (_, res, next) => Patient
-  .findAll({ include: Surgerie })
-  .then((patients) => res.status(200).json(patients))
-  .catch((e) => next(e.message));
-
 plansRouter
   .get('/:id', getPatientsById)
-  .get('/surgery', getAllWithSurgeries)
-  .get('/s', (req, res, next) => {
-    Surgerie.findAll().then((s) => res.status(200).json(s)).catch((e) => next(e))
-  })
-  .get('/ps', (req, res, next) => {
-    PatientSurgeries.findAll().then((s) => res.status(200).json(s)).catch((e) => next(e))
-  });
 
 module.exports = plansRouter;
